a6cd8b7ae9d3e5516528523aed96f36eca0c9324
NIMO <laomms@gmail.com>
2020-09-21 19:51:51 +0700

    modify

--- Call_thiscall/Program.cs
+++ Call_thiscall/Program.cs
@@ -9,16 +9,35 @@ namespace Call_thiscall
 {
     class Program
     {
-        [DllImport("thiscallDll.dll", EntryPoint = "?func_thiscall@TestClass@@QAEHHHHH@Z", CallingConvention = CallingConvention.ThisCall)] //类成员函数
-        private static extern int _func_thiscall(ThisCallClass.TestClass ths, int a, int b, int c, int d);
+        [DllImport("thiscallDll.dll", EntryPoint = "?func_thiscall@TestClass@@QAEHHHHHH@Z", CallingConvention = CallingConvention.ThisCall)] //类成员函数
+        private static extern int func_thiscall(ThisCallClass.TestClass ths, int a, int b, int c, int d, int e);
+        
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+        delegate int func_thiscall3(ThisCallClass.TestClass ths, int a, int b, int c, int d, int e);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        delegate int func_thiscall2( int a, int b, int c, int d, int e);
         static void Main(string[] args)
         {
             ThisCallClass class1 = new ThisCallClass(1);
-            int res= _func_thiscall(ThisCallClass.class1, 3, 4,5,6);
+            int res= func_thiscall(ThisCallClass.class1, 3, 4, 5, 6, 7);
+            Console.WriteLine(res.ToString());
+            IntPtr hDll= LoadLibrary("thiscallDll.dll");
+            func_thiscall3 func3 = (func_thiscall3)Marshal.GetDelegateForFunctionPointer(IntPtr.Add(hDll, 0x11680), typeof(func_thiscall3));
+            res = func3(ThisCallClass.class1, 3, 4, 5, 6, 7);
+            Console.WriteLine(res.ToString());
+            func_thiscall2 func2 = (func_thiscall2)Marshal.GetDelegateForFunctionPointer(IntPtr.Add(hDll, 0x11680), typeof(func_thiscall2));
+            res = func2( 3, 4, 5, 6, 7);
             Console.WriteLine(res.ToString());
             class1.Dispose();
             Console.ReadLine();
         }
 
-     }
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
+        static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string lpFileName);
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+    }
 }
--- thiscallDll/dllmain.cpp
+++ thiscallDll/dllmain.cpp
@@ -3,12 +3,25 @@
 #include <stdio.h>
 
 
-class __declspec(dllexport) TestClass {
-
-public:
-	int __thiscall func_thiscall(int a,int b,int c,int d)
+class __declspec(dllexport) TestClass 
+{  
+public: //11230
+	int __thiscall func_thiscall(int a,int b,int c,int d, int e)  
 	{
-		return a+b+c+d;
+		return a + b + c + d + e;
 	}
 };
 
+
+class TestClass2 //1111D
+{
+public:
+
+private:
+	virtual int __thiscall func_thiscall3(int a, int b, int c, int d, int e);
+};
+
+int __thiscall TestClass2::func_thiscall3(int a, int b, int c, int d, int e)   
+{
+	return a + b + c + d + e ;
+}
